256f0cde111707e3abd8104c08a12e7fa88d7340
navyadhanalakshmi-bandaru <navyadhanalakshmibandaru@gmail.com>
2026-09-23 04:08:29 +0530

    Day 2: Variables and Comments

---
+++ python/Day02.py
@@ -0,0 +1,59 @@
+
+# Day 2: Python Variables and Comments
+
+# 1. Variables
+name = "Navya"
+age = 21
+course = "Python Full Stack"
+cgpa = 9.34
+is_learning = True
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("CGPA:", cgpa)
+print("Currently Learning:", is_learning)
+
+
+# 2. Different Data Types
+print("\n--- Data Types ---")
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_learning))
+
+
+# 3. Single-line Comments
+# This is a single-line comment
+# Comments explain the code and improve readability
+
+print("\nLearning Python!")
+
+
+# 4. Multiline Comments Using # 
+# This is the first line
+# This is the second line
+# This is the third line
+
+print("Python is easy to learn")
+
+
+# 5. Triple-quoted Multiline String
+"""
+This is a multiline string.
+Triple quotes can be used for
+writing multiline text.
+"""
+
+print("Day 2 completed!")
+
+
+# 6. Variables with User Input
+print("\n--- User Input ---")
+
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+
+print("Hello,", user_name)
+print("Your age is:", user_age)
